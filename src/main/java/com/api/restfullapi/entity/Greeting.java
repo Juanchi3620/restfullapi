0d19fa6378e6f30068c3b65e7d0b8475e2fba613
Juanchi3620 <francovargasjuandario@gmail.com>
2023-10-16 10:06:28 -0500

    Add greetings

---
+++ src/main/java/com/api/restfullapi/entity/Greeting.java
@@ -0,0 +1,13 @@
+package com.api.restfullapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greeting {
+    private long id;
+    private String content;
+}
